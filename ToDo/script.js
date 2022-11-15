c009aa67cc8b76366532fc2022c798a0c67d5482
henna storch <henna.storch@gmail.com>
2022-11-15 14:27:51 +0200

    recognizes text

--- ToDo/script.js
+++ ToDo/script.js
@@ -1,20 +1,35 @@
-let text = document.getElementById("task-text-input");
+;
 let addTaskButton = document.getElementById('add-task');
 let saveTaskButton = document.getElementById("save-todo");
 let listBox = document.getElementById('listBox');
 let saveIndex = document.getElementById("saveIndex");
+let completed = document.getElementById("completedList")
+
 
 
 
 addTaskButton.addEventListener('click', function(){
-    text.changeInput
-    console.log(text)
+    //create element where task is stored
     let div = document.createElement('div');
     div.id = "aTask";
     div.classList.add ("aTask")
     listBox.appendChild(div)
-    div.style.backgroundColor = "blue"
-    div.style.height = 135 + 'px' 
+    //first save the text in input
+    let text = document.getElementById("task-text-input").value
     div.innerHTML = text
+    //checkbox when complete task
+    let checkbox = document.createElement('input')
+    checkbox.type = "checkbox"
+    checkbox.id = "taskComplete"
+    div.appendChild(checkbox)
+    //move completed tasks to seperate list
+    checkbox.addEventListener('click', function(){
+        listBox.removeChild(div)
+        let completedTask = document.createElement('div')
+        completedTask.innerHTML = (text)
+        completed.appendChild(completedTask)
+    })
     });
+
+
 
