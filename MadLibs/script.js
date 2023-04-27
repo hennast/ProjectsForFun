0897c1c933be970108acd52d5cac50164156f783
henna storch <henna.storch@gmail.com>
2023-04-27 08:20:15 +0300

    pos check

--- MadLibs/script.js
+++ MadLibs/script.js
@@ -1,11 +1,32 @@
 
+url = "https://api.dictionaryapi.dev/api/v2/entries/en/"
 
-storySpot = document.getElementById('story') //this is the span that the story goes in
-let div = document.createElement('div');
-div.id = "madLib"
-div.classList.add ("madLib")
-storySpot.appendChild(div)
+function idPartofSpeech(word, part) { //this function will use a dictionary api to check that the user part of speech is correct
+  let def = url+word
+  fetch(def)
+  .then(response => response.json())
+    .then(data => {
 
+      console.log(data[0].meanings.length)
+      for (let i = 0; i < data[0].meanings.length; i++){
+        pos1 = data[0].meanings[i].partOfSpeech
+        if (part == pos1){
+          console.log(word + "true")
+          console.log(pos1)
+          break
+        }
+        else {
+          console.log(word + "false")
+          console.log(pos1)
+        }
+      }
+      
+    })
+
+  console.log(word)
+  console.log(part)
+}
+idPartofSpeech("hello", "noun"); //test
 
 
 
@@ -18,18 +39,22 @@ form.addEventListener("submit", test)
               noun = document.getElementById('noun').value; //creates variable for noun itself
               userNoun = document.getElementById('userNoun') // creates variable for location of noun in story
               userNoun.innerHTML = noun //replaces 'noun' placeholder with noun user has entered
+              idPartofSpeech(noun, "noun")
 
               adj = document.getElementById('adjective').value; //creates variable for adj itself
               userAdj = document.getElementById('userAdj') // creates variable for location of adj in story
               userAdj.innerHTML = adj //replaces 'adj' placeholder with adj user has entered
+              idPartofSpeech(adj, "adjective")
 
               person = document.getElementById('person').value; //creates variable for name itself
               userPerson = document.getElementById('userPerson') // creates variable for location of person in story
               userPerson.innerHTML = person //replaces 'person' placeholder with [person] user has entered
+              //no function here, can be creative
 
               verb = document.getElementById('verb').value; //creates variable for verb itself
               userVerb = document.getElementById('userVerb') // creates variable for location of ver in story
               userVerb.innerHTML = verb //replaces 'verb' placeholder with verb user has entered
+              idPartofSpeech(verb, "verb")
 
               place  =  document.getElementById('place').value
               userPlace = document.getElementById('userPlace') // creates variable for location of place in story
