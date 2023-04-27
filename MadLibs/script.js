63c16436ca503a51c0bd10c449e8054c77f9f925
henna storch <henna.storch@gmail.com>
2023-04-27 21:26:32 +0300

    some stuff

--- MadLibs/script.js
+++ MadLibs/script.js
@@ -1,40 +1,54 @@
 
 url = "https://api.dictionaryapi.dev/api/v2/entries/en/"
+let errBox = document.getElementById('errors')
 
 function idPartofSpeech(word, part) { //this function will use a dictionary api to check that the user part of speech is correct
   let def = url+word
   fetch(def)
   .then(response => response.json())
     .then(data => {
-
+      let x = 0
       console.log(data[0].meanings.length)
+    
       for (let i = 0; i < data[0].meanings.length; i++){
         pos1 = data[0].meanings[i].partOfSpeech
         if (part == pos1){
           console.log(word + "true")
-          console.log(pos1)
+          x = 1
           break
         }
         else {
           console.log(word + "false")
-          console.log(pos1)
+          x = 0
         }
       }
-      
+      if(x == 0) {
+        let p = document.createElement('p')
+        errBox.appendChild(p)
+        p.innerHTML = 'please input a valid ' + part
+      }
     })
 
   console.log(word)
   console.log(part)
 }
-idPartofSpeech("hello", "noun"); //test
+
+
+//idPartofSpeech("hello", "adjective"); //test
 
 
 
 
 let form = document.getElementById('libform')
-form.addEventListener("submit", test)
-            function test(e){
+
+
+  form.addEventListener("submit", test)
+          function test(e){
+            //if (errBox.childNodes.length === 0)
+              //{
               e.preventDefault() //keeps page from refreshing
+              
+              
 
               noun = document.getElementById('noun').value; //creates variable for noun itself
               userNoun = document.getElementById('userNoun') // creates variable for location of noun in story
@@ -61,3 +75,4 @@ form.addEventListener("submit", test)
               userPlace.innerHTML = place //replaces 'place' placeholder with place user has entered             
                
             }
+         // }
